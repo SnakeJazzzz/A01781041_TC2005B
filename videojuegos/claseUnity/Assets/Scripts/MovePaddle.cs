51dc762e10cca9ed80d333ff6d6b9f4833337e1f
SnakeJazzzz <michael2506@icloud.com>
2024-02-20 18:31:34 -0600

    Some notes and unity first class

---
+++ videojuegos/claseUnity/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,31 @@
+//Script to move the paddle in pong game
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+    public float speed;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(moveUp))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if(Input.GetKey(moveDown))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        
+    }
+}
